dac21e0ffecb712876dbf68cde4d64eb536e7b83
Jeff Smith <whydoubt@gmail.com>
2018-07-15 01:35:47 -0500

    Create core class and have the main class use it

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,8 @@
 public class App {
     public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        Queens q = new Queens(N);
+        q.solve();
+        q.reportAlgebraic();
     }
 }
---
+++ src/main/java/Queens.java
@@ -0,0 +1,13 @@
+public class Queens {
+    private int N;
+
+    Queens(int N) {
+        this.N = N;
+    }
+
+    public void solve() {
+    }
+
+    public void reportAlgebraic() {
+    }
+}
